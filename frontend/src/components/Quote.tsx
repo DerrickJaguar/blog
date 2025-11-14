585dfe5230676f29743bd78f3dc93a774dde6e32
DerrickJaguar <derrric65@gmail.com>
2025-11-14 16:15:52 +0300

    commit

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,11 +3,10 @@ export function Quote() {
     <div className="bg-slate-100 h-screen flex justify-center items-center">
       <div className="flex flex-col justify-start  ">
         <div className="font-sans text-3xl font-semibold max-w-lg">
-          "The customer service I received was exceptional. The support team
-          went above and beyond to address my concerns."
+          "Africa Daily Times - Your trusted source for insightful news, opinions, and stories from across Africa and beyond."
         </div>
-        <div className="text-2xl mt-4 font-medium"> Jules</div>
-        <div className="text-gray-500 font-medium"> CEO</div>
+        <div className="text-2xl mt-4 font-medium"> Africa Daily Times</div>
+        <div className="text-gray-500 font-medium"> Informing Africa, Inspiring the World</div>
       </div>
     </div>
   );
